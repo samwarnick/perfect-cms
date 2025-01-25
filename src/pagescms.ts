01e0dcae1b88f992a16c82825a2bc3caf46a0887
Sam Warnick <sam@warnick.me>
2025-01-25 12:47:56 -0500

    Update url

--- src/pagescms.ts
+++ src/pagescms.ts
@@ -1,3 +1,3 @@
 export function generateEditUrl(filename: string) {
-	return `https://app.pagescms.org/${Bun.env.GITHUB_OWNER}/${Bun.env.GITHUB_REPO}/main/content/posts/edit/${encodeURIComponent(filename)}`;
+	return `https://app.pagescms.org/${Bun.env.GITHUB_OWNER}/${Bun.env.GITHUB_REPO}/main/collection/posts/edit/${encodeURIComponent(filename)}`;
 }
